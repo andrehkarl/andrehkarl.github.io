bc92e38b8c3aaeabf9b4ad6efcb9767b116934d7
Andre Karl <andre.karl@seequestor.com>
2025-10-06 20:59:53 +0100

    Removing things causing trouble on the UC page

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,30 +9,30 @@
 (function() {
   "use strict";
 
-  /**
-   * Apply .scrolled class to the body as the page is scrolled down
-   */
-  function toggleScrolled() {
-    const selectBody = document.querySelector('body');
-    const selectHeader = document.querySelector('#header');
-    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
-  }
+//  /**
+//   * Apply .scrolled class to the body as the page is scrolled down
+//   */
+//  function toggleScrolled() {
+//    const selectBody = document.querySelector('body');
+//    const selectHeader = document.querySelector('#header');
+//    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+//    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+//  }
+//
+//  document.addEventListener('scroll', toggleScrolled);
+//  window.addEventListener('load', toggleScrolled);
 
-  document.addEventListener('scroll', toggleScrolled);
-  window.addEventListener('load', toggleScrolled);
-
-  /**
-   * Mobile nav toggle
-   */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
-
-  function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
-  }
-  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+//  /**
+//   * Mobile nav toggle
+//   */
+//  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+//
+//  function mobileNavToogle() {
+//    document.querySelector('body').classList.toggle('mobile-nav-active');
+//    mobileNavToggleBtn.classList.toggle('bi-list');
+//    mobileNavToggleBtn.classList.toggle('bi-x');
+//  }
+//  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
 
   /**
    * Hide mobile nav on same-page/hash links
@@ -40,7 +40,7 @@
   document.querySelectorAll('#navmenu a').forEach(navmenu => {
     navmenu.addEventListener('click', () => {
       if (document.querySelector('.mobile-nav-active')) {
-        mobileNavToogle();
+       // mobileNavToogle();
       }
     });
 
@@ -68,26 +68,26 @@
     });
   }
 
-  /**
-   * Scroll top button
-   */
-  let scrollTop = document.querySelector('.scroll-top');
+ // /**
+ //  * Scroll top button
+ //  */
+ // let scrollTop = document.querySelector('.scroll-top');
 
-  function toggleScrollTop() {
-    if (scrollTop) {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-    }
-  }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  });
-
-  window.addEventListener('load', toggleScrollTop);
-  document.addEventListener('scroll', toggleScrollTop);
+ // function toggleScrollTop() {
+ //   if (scrollTop) {
+ //     window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+ //   }
+ // }
+ // scrollTop.addEventListener('click', (e) => {
+ //   e.preventDefault();
+ //   window.scrollTo({
+ //     top: 0,
+ //     behavior: 'smooth'
+ //   });
+ // });
+ //
+ // window.addEventListener('load', toggleScrollTop);
+ // document.addEventListener('scroll', toggleScrollTop);
 
   /**
    * Animation on scroll function and init
